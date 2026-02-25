7680513cd6781b248724dbe3e552107755e09e6a
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-02-25 10:30:33 +0200

    Added: variables, getter Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,35 @@
 package model;
 
+//modelu klase
 public class Student {
-
+	//1. mainigie
+	private long stud_id;
+	private String name;
+	private String surname;
+	private String person_code;
+	
+	//2. get funkcijas
+	public long get_stud_id() {
+		return stud_id;
+	}
+	
+	public String get_name() {
+		return name;
+	}
+	
+	public String get_surname() {
+		return surname;
+	}
+	
+	public String get_person_code() {
+		return person_code;
+	}
+	
+	
+	
+	//3. set funkcijas
+	//4.1 bezargumenta funkcijas
+	//4.2 argumenta funkcijas
+	//5. toString funkcijas
+	//6. visas parejas funkcijas pec nepieciesamibas
 }
