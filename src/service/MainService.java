976945bde9cfebceebd55ef596c599e2b005717f
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-02-25 12:09:55 +0200

    Add: Prof MainService, needs fixing

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,7 +1,8 @@
 package service;
 
 import model.Student;
-
+import model_enums.ProfDegree;
+import model.Professor;
 public class MainService {
 
 	public static void main(String[] args) {
@@ -12,6 +13,15 @@ public class MainService {
 		System.out.println(stud2);
 
 		Student stud3 = new Student("Gint2", "SmiLt", null);
+		System.out.println(stud3);
+		
+		Professor prof1 = new Professor();
+		System.out.println(stud1);
+		
+		Professor prof2 = new Professor("Austr", "Zeid", "124515-67891", ProfDegree.bachelor);
+		System.out.println(stud2);
+
+		Professor prof3 = new Professor("Gint2", "SmiLt", null, null);
 		System.out.println(stud3);
 	}
 }
