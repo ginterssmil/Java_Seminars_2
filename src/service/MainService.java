308b4cbb81d5e3aa99c55100f2d1b75b02f60691
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-02-25 11:41:22 +0200

    Add: MainService Student class test

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,5 +1,17 @@
 package service;
 
+import model.Student;
+
 public class MainService {
 
+	public static void main(String[] args) {
+		Student stud1 = new Student();
+		System.out.println(stud1);
+		
+		Student stud2 = new Student("Gint", "Smilt", "123415-51034");
+		System.out.println(stud2);
+
+		Student stud3 = new Student("Gint2", "SmiLt", null);
+		System.out.println(stud3);
+	}
 }
