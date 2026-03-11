28f62fb7318b82365df246822bf914c000be934a
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-03-11 09:08:30 +0200

    New class person

---
+++ src/model/Person.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Person {
+	private String name;
+	private String surname;
+	private String person_code;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getPersonCode() {
+		return person_code;
+	}
+	
+	
+	public void setName(String input_name) {
+		if( (input_name != null) && (!input_name.isEmpty()) && (input_name.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?")) ) {
+			name = input_name;
+		}
+		else {
+			name = "Unknown";
+		}
+	}
+	
+	public void setSurname(String input_surname) {
+		if( (input_surname != null) && (!input_surname.isEmpty()) && (input_surname.matches("[A-Z]{1}[a-z]{2,15}([-]{1}[A-Z]{1}[a-z]{2,15})?")) ) {
+			surname = input_surname;
+		}
+		else {
+			surname = "Unknown";
+		}
+	}
+	
+	public void setPersonCode(String input_person_code) {
+		if( (input_person_code != null) && (!input_person_code.isEmpty()) && (input_person_code.matches("[0-9]{6}[-]{1}[0-9]{5}")) ) {
+			person_code = input_person_code;
+		}
+		else {
+			person_code = "Unknown";
+		}
+	}
+	
+	
+	
+}
