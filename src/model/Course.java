bdfe28ffb42b62efa753f443c7ff5839376eceef
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-03-11 07:52:03 +0200

    Edit: added new class Course

---
+++ src/model/Course.java
@@ -0,0 +1,59 @@
+package model;
+
+import model_enums.ProfDegree;
+
+public class Course {
+	private long courseId;
+	private String title;
+	private byte creditPoints;
+	private Professor professor;
+	
+	private static long counter = 20000;
+	
+	//getters
+	public long getCourseId() {
+		return courseId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public byte getCreditPoints() {
+		return creditPoints;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+	
+	
+	//setters
+	public void setCourseId() {
+		courseId = counter;
+		counter++;
+	}
+
+	public void setTitle(String inputTitle) {
+		if ( (inputTitle != null) && (inputTitle.matches("[A-Z]{1}[A-Za-z0-9{1,40}"))) {
+			title = inputTitle;
+		}
+		else {
+			title = "Unknown";
+		}
+	}
+
+	public void setCreditPoints(byte inputCreditPoints) {
+		if ((inputCreditPoints >= 1) && (inputCreditPoints <=30)) {
+			creditPoints = inputCreditPoints;
+		}
+	}
+
+	public void setProfessor(Professor inputProfessor) {
+		if ( (inputProfessor != null) ) {
+			professor = inputProfessor;
+		}
+	}
+
+	
+}
